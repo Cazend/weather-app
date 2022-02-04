0b15f5282705272612ab90e25363bb8bf7ffd10d
Cazend <hiresh1010@gmail.com>
2022-02-04 19:35:44 +0100

    fix input search bar

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+if (typeof jQuery == 'undefined') {
+  console.log('Unable to load jQuery');
+} else {
+  console.log('jQuery has been loaded.');
+}
+
+$('.dropdown-menu').hide();
+
 $(".dropdown-button").on("click", function(){
   $(".dropdown-menu").slideToggle();
 });
