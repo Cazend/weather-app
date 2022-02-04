fb64b090effe7a8caa4f4776c43c11963077a204
Cazend <hiresh1010@gmail.com>
2022-02-04 18:39:15 +0100

    fix dropdown met jquery

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-/*document.addEventListener("click", e => {
-    const isDropdownButton = e.target.matches("[data-dropdown-button]")
-    if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
-  
-    let currentDropdown
-    if (isDropdownButton) {
-      currentDropdown = e.target.closest("[data-dropdown]")
-      currentDropdown.classList.toggle("active")
-    }
-  
-    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
-      if (dropdown === currentDropdown) return
-      dropdown.classList.remove("active")
-    })
-  })*/
-
-function myFunction() {
-  dropdown.classList.remove("active");
-}
+$(".dropdown-button").on("click", function(){
+  $(".dropdown-menu").slideToggle();
+});
