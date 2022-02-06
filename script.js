f21f2cd5f898038da9650f0770bd58eaa4cb16ff
denzymeow <99141863+denzymeow@users.noreply.github.com>
2022-02-07 00:45:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
   $('.dropdown-menu').hide();
 
   $(".dropdown-button").on("click", function(){
     $(".dropdown-menu").slideToggle("fast");
   });
-
+  
   $("#searchInput").on("keyup", function() {
       var value = $(this).val().toLowerCase();
       $("#locationList a").css("display", function() {
@@ -12,21 +13,56 @@ $(document).ready(function(){
       });
   });
 
-  /*var getData = function(area){
-    $.getJSON("https://data.buienradar.nl/2.0/feed/json", function(json) {
-      //var tmpData = JSON.parse(json);
-      console.log("JSON Data: " + json[2].buienradar[0])
-    });
-    return false;
-  }
-*/
-  $("#den-haag").on("click", function(){
-   //getData("Den Haag");
-   $.get("https://data.buienradar.nl/2.0/feed/json", function(json) {
-     var tempData = (json);
-     alert("Data: " + tempData);
-   })
-    $('.dropdown-menu').hide();
-  })
-
+  $("#search-query").submit(function(event) { 
+      performSearch(event); });
 });
+
+function performSearch(event) {
+  var request;
+  event.preventDefault();
+  $(".location").text("Searching...");
+  $(".temp-value").text();
+  $(".temp-desc").text();
+  $(".humidity").text();
+  $(".wind").text();
+
+
+  request = $.ajax({
+      url: 'https://api.openweathermap.org/data/2.5/weather',
+      type: "GET",
+      data: { q: $("#searchInput").val(), 
+              appid: 'de2d41b23483456da8e390456f8b2f4b',
+              units: 'metric'}
+  });
+
+
+  request.done(function (response){
+      formatSearchResults(response);
+  });
+  
+
+  request.fail(function (){
+      $(".location").text("N/A");
+      $(".temp-desc").text();
+      $(".temp-value").text();
+      $(".humidity").text();
+      $(".wind").text();
+    
+  });
+
+}
+
+function formatSearchResults(jsonObject) {
+  
+  var location = jsonObject.name;
+  location = location + ", " + jsonObject.sys.country;
+  var temp_desc = jsonObject.weather[0].main;
+  var temp_value = jsonObject.main.temp;
+  var humidity = jsonObject.main.humidity;
+  var wind = jsonObject.wind.speed;
+  $(".location").text(location);
+  $(".temp-desc").text(temp_desc);
+  $(".temp-value").text(temp_value);  
+  $(".humidity").text(humidity+"%");
+  $(".wind").text(wind+" km/h");
+}
