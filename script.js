8b5dc3f6a266acbf513e5f322346dd5aa426ccaa
Cazend <hiresh1010@gmail.com>
2022-02-06 19:18:28 +0100

    make search list clickable

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-if (typeof jQuery == 'undefined') {
-  console.log('Unable to load jQuery');
-} else {
-  console.log('jQuery has been loaded.');
-}
-
-$('.dropdown-menu').hide();
-
-$(".dropdown-button").on("click", function(){
-  $(".dropdown-menu").slideToggle();
-});
-
 $(document).ready(function(){
+  $('.dropdown-menu').hide();
+
+  $(".dropdown-button").on("click", function(){
+    $(".dropdown-menu").slideToggle("fast");
+  });
+
   $("#searchInput").on("keyup", function() {
       var value = $(this).val().toLowerCase();
-      $("#locationList p").css("display", function() {
-          return this.innerText.toLowerCase().indexOf(value) > -1 ? "":"none"
+      $("#locationList a").css("display", function() {
+         return this.innerText.toLowerCase().indexOf(value) > -1 ? "":"none"
       });
   });
 });
