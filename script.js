ebaa752bf3b6b487c98bc30d91c96caf90cd8a85
Cazend <hiresh1010@gmail.com>
2022-02-06 18:58:11 +0100

    WIP search function

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ $('.dropdown-menu').hide();
 $(".dropdown-button").on("click", function(){
   $(".dropdown-menu").slideToggle();
 });
+
+$(document).ready(function(){
+  $("#searchInput").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#locationList p").css("display", function() {
+          return this.innerText.toLowerCase().indexOf(value) > -1 ? "":"none"
+      });
+  });
+});
