bb623373161140baa27f8c7c42ccf8cdbe6ae5d5
Cazend <hiresh1010@gmail.com>
2022-02-13 14:40:52 +0100

    begin aan forecast

--- script.js
+++ script.js
@@ -17,61 +17,8 @@ $(document).ready(function() {
 
   $("#search-query").submit(function(event) { 
     performSearch(event, document.getElementById("searchInput").value); });
-  
-    var autocomplete;
-
-    function initAutocomplete() {
-      // Create the autocomplete object, restricting the search to geographical
-      // location types.
-      autocomplete = new google.maps.places.Autocomplete(document.getElementById('searchInput')),
-          {types: ['geocode']}
-
-      // When the user selects an address from the dropdown, populate the address
-      // fields in the form.
-      autocomplete.addListener('place_changed', fillInAddress);
-    }
-
-    function fillInAddress() {
-      // Get the place details from the autocomplete object.
-      var place = autocomplete.getPlace();
-
-      for (var component in componentForm) {
-        document.getElementById(component).value = '';
-        document.getElementById(component).disabled = false;
-      }
-
-      // Get each component of the address from the place details
-      // and fill the corresponding field on the form.
-      for (var i = 0; i < place.address_components.length; i++) {
-        var addressType = place.address_components[i].types[0];
-        if (componentForm[addressType]) {
-          var val = place.address_components[i][componentForm[addressType]];
-          document.getElementById(addressType).value = val;
-        }
-      }
-    }
-
-    // Bias the autocomplete object to the user's geographical location,
-    // as supplied by the browser's 'navigator.geolocation' object.
-    function geolocate() {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-          var geolocation = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          };
-          var circle = new google.maps.Circle({
-            center: geolocation,
-            radius: position.coords.accuracy
-          });
-          autocomplete.setBounds(circle.getBounds());
-        });
-      }
-    }
-  let loc;
 
   function performSearch(event, location) {
-    let loc = location
     var request;
     event.preventDefault();
     $(".location").text("Searching...");
@@ -85,14 +32,13 @@ $(document).ready(function() {
     request = $.ajax({
       url: 'https://api.openweathermap.org/data/2.5/weather',
       type: "GET",
-      data: { q: loc, 
+      data: { q: location, 
       appid: 'de2d41b23483456da8e390456f8b2f4b',
       units: 'metric'}
     });
     
-    
     request.done(function (response){
-      formatSearchResults(response);
+      formatSearchResults(0, response);
       console.log(request);
     });
     
@@ -104,19 +50,28 @@ $(document).ready(function() {
       $(".temp-value").html("∞ <img src='icons/thermometer-celsius.svg'></img>"); 
       $(".humidity").text("N/A");
       $(".wind").text("N/A");
-      
     });
-    
   }
 
-  function formatSearchResults(jsonObject) {
+  function formatSearchResults(type, jsonObject) {
     
-    var location = jsonObject.name;
-    var temp_weather_id = jsonObject.weather[0].id;
-    var temp_desc = jsonObject.weather[0].main;
-    var temp_value = jsonObject.main.temp;
-    var humidity = jsonObject.main.humidity;
-    var wind = jsonObject.wind.speed;
+    var location, temp_weather_id, temp_desc, temp_value, humidity, wind;
+    if(type == 0) {
+      location = jsonObject.name;
+      temp_weather_id = jsonObject.weather[0].id;
+      temp_desc = jsonObject.weather[0].main;
+      temp_value = jsonObject.main.temp;
+      humidity = jsonObject.main.humidity;
+      wind = jsonObject.wind.speed;
+    } else {
+      console.log(jsonObject);
+      location = jsonObject.city.name;
+      //temp_weather_id = jsonObject.weather[0].id;
+      //temp_desc = jsonObject.weather[0].main;
+      //temp_value = jsonObject.main.temp;
+      //humidity = jsonObject.main.humidity;
+      wind = jsonObject.list[0].wind.speed;
+    }
 
     if(temp_desc != "Atmosphere" && "Clear" && "Clouds") {
       $("#weather-icon-img").attr("src", "icons/" + temp_desc + ".svg");
@@ -154,7 +109,8 @@ $(document).ready(function() {
         var lat = crd.latitude.toString();
         var lng = crd.longitude.toString();
         var coordinates = [lat, lng];
-        getCity(coordinates);
+        getForecast(coordinates);
+        //getCity(coordinates);
         return;
     }
 
@@ -180,9 +136,46 @@ $(document).ready(function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
             var response = JSON.parse(xhr.responseText);
             var city = response.address.city;
-            performSearch(e, city); // tijdelijk, moet nog verbeterd worden
+            //performSearch(e, city);
+            getForecast(city);
             return;
         }
     }
+  }
+
+  function getForecast(coordinates) {
+    var request;
+    $(".location").text("Searching...");
+    $("#weather-icon-img").attr("src", "icons/loading.svg");
+    $(".temp-value").text();
+    $(".temp-desc").text();
+    $(".humidity").text();
+    $(".wind").text();
+    
+    var la = coordinates[0];
+    var lo = coordinates[1];
+
+    request = $.ajax({
+      url: '//api.openweathermap.org/data/2.5/forecast',
+      dataType: 'json',
+      type: "GET",
+      data: { lat: la, lon: lo, 
+      appid: 'de2d41b23483456da8e390456f8b2f4b',
+      units: 'metric'}
+    });
+    
+    request.done(function (response){
+      formatSearchResults(1, response);
+      console.log(request);
+    });
+  
+    request.fail(function (){
+      $(".location").text("Invalid location");
+      $("#weather-icon-img").attr("src", "icons/not-available.svg");
+      $(".temp-desc").text("N/A");
+      $(".temp-value").html("∞ <img src='icons/thermometer-celsius.svg'></img>"); 
+      $(".humidity").text("N/A");
+      $(".wind").text("N/A");
+    });
   }
 });
